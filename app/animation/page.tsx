2668df6ac7665e143d9476804a6e741da48d327a
Ilia Gorshenev <94396603+IliaGorshenev@users.noreply.github.com>
2024-09-24 20:28:24 +0300

    adding animation with letters

---
+++ app/animation/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Script from "next/script";
+import React from "react";
+import HindiAlphabetAnimation from "../components/HindiAlphabetAnimation";
+
+const Animation: React.FC = () => {
+  return (
+    <div>
+      <Script
+        strategy="beforeInteractive"
+        src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+        type="module"
+      />
+      {/* <h1>Welcome to My Next.js App</h1> */}
+      <HindiAlphabetAnimation />
+    </div>
+  );
+};
+
+export default Animation;
---
+++ app/components/HindiAlphabetAnimation.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+
+const HindiAlphabetAnimation: React.FC = () => {
+  const hindiAlphabet = [
+    "अ",
+    "आ",
+    "इ",
+    "ई",
+    "उ",
+    "ऊ",
+    "ऋ",
+    "ए",
+    "ऐ",
+    "ओ",
+    "औ",
+    "अं",
+    "अः",
+    "क",
+    "ख",
+    "ग",
+    "घ",
+    "ङ",
+    "च",
+    "छ",
+    "ज",
+    "झ",
+    "ञ",
+    "ट",
+    "ठ",
+    "ड",
+    "ढ",
+    "ण",
+    "त",
+    "थ",
+    "द",
+    "ध",
+    "न",
+    "प",
+    "फ",
+    "ब",
+    "भ",
+    "म",
+    "य",
+    "र",
+    "ल",
+    "व",
+    "श",
+    "ष",
+    "स",
+    "ह",
+    "क्ष",
+    "त्र",
+    "ज्ञ",
+  ];
+  const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentLetterIndex(
+        (prevIndex) => (prevIndex + 1) % hindiAlphabet.length
+      );
+    }, 900); // Change letter every 1 second
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <span style={{ fontSize: "10rem" }}>
+        {hindiAlphabet[currentLetterIndex]}
+      </span>
+    </div>
+  );
+};
+
+export default HindiAlphabetAnimation;
