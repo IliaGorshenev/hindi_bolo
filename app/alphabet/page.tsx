d96424a8753463dfbf2c0dd4b87192169acee74e
Ilia Gorshenev <94396603+IliaGorshenev@users.noreply.github.com>
2024-09-24 19:56:37 +0300

    adding animation

--- app/alphabet/page.tsx
+++ app/alphabet/page.tsx
@@ -1,8 +1,23 @@
-
-import React from 'react';
+import Script from "next/script";
+import React from "react";
 
 const Alphabet: React.FC = () => {
-  return <h1>New Page</h1>;
+  return (
+    <>
+      <div>
+        <Script
+          strategy="beforeInteractive"
+          src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+          type="module"
+        />
+        <dotlottie-player
+          autoplay
+          src="https://lottie.host/f11d4391-2afb-4c1c-a917-6ef3beedce21/QPxW70TFdZ.json"
+          style={{ width: "auto", height: "100%" }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Alphabet;
