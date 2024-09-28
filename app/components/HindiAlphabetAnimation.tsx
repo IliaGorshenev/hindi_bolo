3946ee6e9c4f99f853641ddcfdc54f22e1ddb572
Ilia Gorshenev <94396603+IliaGorshenev@users.noreply.github.com>
2024-09-28 12:44:07 +0300

    change

--- app/components/HindiAlphabetAnimation.tsx
+++ app/components/HindiAlphabetAnimation.tsx
@@ -188,7 +188,7 @@ const HindiAlphabetAnimation: React.FC = () => {
       setCurrentLetterIndex(
         (prevIndex) => (prevIndex + 1) % hindiAlphabet.length
       );
-    }, 1800); // Change letter every 1 second
+    }, 4000); // Change letter every 1 second
 
     return () => clearInterval(interval);
   }, []);
